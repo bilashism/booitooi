6ec7d895a4e5a039e08b68936ab148a75feda788
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-07-15 07:37:52 +0600

    feat: added simple navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -17,18 +17,32 @@ export const Navbar = () => {
     <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
       <div className="h-full w-full bg-white/60">
         <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+          <Link
+            to="/"
+            className="inline-flex justify-center items-center px-2 py-2 w-8 h-8 shadow-lg shadow-orange-400 font-bold italic bg-purple-600 text-white rounded-full uppercase"
+          >
+            <span>BT</span>
+          </Link>
           <div>
-            <ul className="flex items-center">
-              <li>
+            <ul className="flex items-center gap-3">
+              <li className="hover:underline underline-offset-8 hover:text-purple-600 transition-colors active:text-purple-800">
                 <Link to="/">Home</Link>
               </li>
-              <li>
-                {!user?.email ? (
-                  <>
+              <li className="hover:underline underline-offset-8 hover:text-purple-600 transition-colors active:text-purple-800">
+                <Link to="/books">All Books</Link>
+              </li>
+
+              {!user?.email ? (
+                <>
+                  <li className="hover:underline underline-offset-8 hover:text-purple-600 transition-colors active:text-purple-800">
                     <Link to="/login">Login</Link>
+                  </li>
+                  <li className="hover:underline underline-offset-8 hover:text-purple-600 transition-colors active:text-purple-800">
                     <Link to="/signup">Signup</Link>
-                  </>
-                ) : (
+                  </li>
+                </>
+              ) : (
+                <li className="hover:underline underline-offset-8 hover:text-purple-600 transition-colors active:text-purple-800">
                   <button
                     type="button"
                     onClick={handleLogOut}
@@ -36,8 +50,8 @@ export const Navbar = () => {
                   >
                     Logout
                   </button>
-                )}
-              </li>
+                </li>
+              )}
             </ul>
           </div>
         </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-// for https certificate
-import mkcert from "vite-plugin-mkcert";
+/* eslint-disable import/no-extraneous-dependencies */
 // for localhost url
-import dns from "dns";
-dns.setDefaultResultOrder("verbatim");
+import dns from 'dns';
+
+import { defineConfig } from 'vite';
+// for https certificate
+import mkcert from 'vite-plugin-mkcert';
+
+import react from '@vitejs/plugin-react';
+
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +16,6 @@ export default defineConfig({
   server: {
     https: true,
     host: true,
-    port: 3000
-  }
+    port: 3000,
+  },
 });
